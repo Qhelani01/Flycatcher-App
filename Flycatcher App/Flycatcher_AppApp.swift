15e121dca4e09b08b381e82090bea80521c07ffb
Qhelani01 <qhestoemoyo@gmail.com>
2025-07-01 13:49:35 -0700

    Initial Commit

---
+++ Flycatcher App/Flycatcher_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flycatcher_AppApp.swift
+//  Flycatcher App
+//
+//  Created by Qhelani Moyo on 7/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Flycatcher_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Flycatcher AppTests/Flycatcher_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Flycatcher_AppTests.swift
+//  Flycatcher AppTests
+//
+//  Created by Qhelani Moyo on 7/1/25.
+//
+
+import Testing
+@testable import Flycatcher_App
+
+struct Flycatcher_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
