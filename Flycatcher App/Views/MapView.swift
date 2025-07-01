89466d7c8854c6c9bb0f953287ae3b15e48ad686
Qhelani01 <qhestoemoyo@gmail.com>
2025-07-01 15:20:57 -0700

    Reorganized project structure into Models, Views, and ViewModels folders

---
+++ Flycatcher App/Views/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  Flycatcher App
+//
+//  Created by Qhelani Moyo on 7/1/25.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
